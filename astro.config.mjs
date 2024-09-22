746fdc5e4618a89f4c4100d2360a323c936cda22
Andres Florez <ramiro.florez@bemyself.com>
2024-09-21 21:30:41 -0500

    fix title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Senioland",
+      title: "Seniorland",
       social: {
         github: "https://github.com/andresflorez16/seniorland",
       },
